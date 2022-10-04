c7d2caf680f80c2b12f356def7aa397b46ae7cee
Avinash kumar <avinash.11906852@lpu.in>
2022-10-04 23:31:32 +0530

    Delete Python directory

--- Python/Basics loop programm.py
+++
@@ -1,5 +0,0 @@
-Cars = ["Buggati", "bently", "Audi"]
-for x in Cars:
-  if x == "bently":
-    break
-  print(x) 
--- Python/Odd and Even.py
+++
@@ -1,5 +0,0 @@
-Number = int(input("Enter a number :-"))
-if (Number %2==0 ) :
-print("This number is even")
-else:
-print("This number is odd")
